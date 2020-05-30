50510169ec24732951979b1d93597c49b86b463e
Linda Wang <lindawang256@gmail.com>
2020-05-29 18:22:52 -0700

    Add toggle checkbox event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  password = 'my password';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onButtonClick() {
+    this.password = 'a new password';
+    console.log('Button is clicked!!');
+  }
+  toggleCheckBox(box) {
+    console.log('box', box);
+    this[box] = !this[box];
+    console.log('this[box]', this[box]);
+    console.log('this.useSymbols', this.useSymbols);
+    console.log('this.useNumbers', this.useNumbers);
+  }
 }
